a20244c5c608670e672df046d8055800f5598f87
jkcarlsson85 <jkcarlsson85@gmail.com>
2017-07-30 15:03:37 +0200

    Send email to bookings email as well as the booker

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -2,9 +2,10 @@ class BookingMailer < ApplicationMailer
  
   def booking_email(booking)
     @booking = booking
+    subject_prefix = Rails.env.production? ? "" : "TEST - "
     mail(
       from: "Grebbans <noreply@grebbans.se>", 
-      to: @booking.email, 
-      subject: "Bokningsbekräftelse #{@booking.event.name} #{@booking.event.date.strftime('%F')}")
+      to: [@booking.email, "bookings@grebbans.se"], 
+      subject: subject_prefix + "Bokningsbekräftelse #{@booking.event.name} #{@booking.event.date.strftime('%F')}")
   end
 end
