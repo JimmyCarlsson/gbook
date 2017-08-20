f762d0d4a004a4093e5762c8e67ddfbd5d213117
jkcarlsson85 <jkcarlsson85@gmail.com>
2017-08-20 10:41:05 +0200

    Enable public access for testing purposes

--- app/controllers/v1/events_controller.rb
+++ app/controllers/v1/events_controller.rb
@@ -1,9 +1,9 @@
 class V1::EventsController < V1::BaseController
   before_action :authenticate_admin_from_token! #, except: [:index, :show, :get_related_resource]
-  before_action :authenticate_admin! #, except: [:index, :show, :get_related_resource]
+  #before_action :authenticate_admin! #, except: [:index, :show, :get_related_resource]
   #
   ## USE THIS LINE WHEN GOING LIVE - WILL MAKE IT POSSIBLE FOR USERS NOT LOGGED IN TO SEE EVENTS AND CREATE BOOKINGS!
-  #before_action :authenticate_admin! , except: [:index, :show, :get_related_resource]
+  before_action :authenticate_admin! , except: [:index, :show, :get_related_resource]
   def index
     if context[:current_admin].nil?
       events = Event.where("date <= ?", Date.today).order(:date)
