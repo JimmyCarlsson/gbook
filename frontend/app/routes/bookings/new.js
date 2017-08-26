565e6cde1926ae182385b55d73de4a23b4a49819
jkcarlsson85 <jkcarlsson85@gmail.com>
2017-08-26 11:05:33 +0200

    Add information on what happens when you confirm an order

--- frontend/app/routes/bookings/new.js
+++ frontend/app/routes/bookings/new.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
+    console.log('event:', this.modelFor('events/show'));
+    if (!this.modelFor('events/show')){
+      this.transitionTo('events');
+    }
     return this.store.createRecord('booking', {
       event: this.modelFor('events/show')
     });
