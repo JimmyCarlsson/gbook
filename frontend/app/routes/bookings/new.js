1b03f13468c3050885c0985afb935a203ef633d5
jkcarlsson85 <jkcarlsson85@gmail.com>
2017-08-26 12:59:05 +0200

    Update booking confirmation page and link event straight to booking if not admin

--- frontend/app/routes/bookings/new.js
+++ frontend/app/routes/bookings/new.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    console.log('event:', this.modelFor('events/show'));
     if (!this.modelFor('events/show')){
       this.transitionTo('events');
     }
--- frontend/app/routes/events/show.js
+++ frontend/app/routes/events/show.js
@@ -1,8 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  session: Ember.inject.service(),
 
   model(params) {
     return this.store.findRecord('event', params.id);
+  },
+  setupController(model) {
+    this.set('model', model);
+
+    if (!this.get('session.isAuthenticated')){
+      this.transitionTo('bookings.new');
+    }
   }
 });
