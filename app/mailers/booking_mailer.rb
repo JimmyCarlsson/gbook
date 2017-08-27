38aeb0ff850870d6327d0fe263aa7b2048362bb3
jkcarlsson85 <jkcarlsson85@gmail.com>
2017-08-27 11:28:48 +0200

    Small gui updates and add ticket and invoice as attachments to email

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -2,10 +2,13 @@ class BookingMailer < ApplicationMailer
  
   def booking_email(booking)
     @booking = booking
+    attachments["Faktura_#{booking.event.name}_#{booking.event.date.strftime("%Y%m%d")}_#{booking.id}.pdf"] = InvoicePdf.new(booking).render
+    attachments["Biljett_#{booking.event.name}_#{booking.event.date.strftime("%Y%m%d")}_#{booking.id}.pdf"] = TicketPdf.new(booking).render
     subject_prefix = Rails.env.production? ? "" : "TEST - "
     mail(
       from: "Grebbans <noreply@grebbans.se>", 
-      to: [@booking.email, "bookings@grebbans.se"], 
+      to: @booking.email,
+      bcc: "bookings@grebbans.se",
       subject: subject_prefix + "Bokningsbekräftelse #{@booking.event.name} #{@booking.event.date.strftime('%F')}")
   end
 end
--- frontend/app/components/events/edit-form.js
+++ frontend/app/components/events/edit-form.js
@@ -2,6 +2,30 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  priceWithDedicatedTax: Ember.computed.sum('model.tax25', 'model.tax12', 'model.tax6'),
+  priceWithoutDedicatedTax: Ember.computed('priceWithDedicatedTax', 'model.price', function(){
+    return this.get('model.price') - this.get('priceWithDedicatedTax');
+  }),
+
+  percentageTax25: Ember.computed('model.tax25', 'model.price', function(){
+    return this.calculatePercent(this.get('model.tax25'), this.get('model.price')).toFixed(2);
+  }),
+
+  percentageTax12: Ember.computed('model.tax12', 'model.price', function(){
+    return this.calculatePercent(this.get('model.tax12'), this.get('model.price')).toFixed(2);
+  }),
+
+  percentageTax6: Ember.computed('model.tax6', 'model.price', function(){
+    return this.calculatePercent(this.get('model.tax6'), this.get('model.price')).toFixed(2);
+  }),
+
+  calculatePercent(part, whole) {
+    if (part < 1 || whole < 1 ){
+      return 0;
+    }
+    return (part / whole) * 100
+  },
+
   actions: {
     save() {
       var that = this;
