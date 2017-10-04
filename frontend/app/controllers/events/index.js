14296e712edfff974e587ab633c3965cb9233900
jkcarlsson85 <jkcarlsson85@gmail.com>
2017-10-04 20:38:35 +0200

    Remove redundant text with admin ticket info, add aggregated number for admins for entire event filter

--- frontend/app/controllers/events/index.js
+++ frontend/app/controllers/events/index.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  session: Ember.inject.service()
+  session: Ember.inject.service(),
+  totalTicketsArray: Ember.computed.mapBy('model', 'bookedSeats'),
+  totalTickets: Ember.computed.sum('totalTicketsArray')
 });
