4938624bc1c147e1baf14f2a929cceb8290ca3fa
jkcarlsson85 <jkcarlsson85@gmail.com>
2017-10-04 20:40:15 +0200

    Add total seats to aggregated presentation

--- frontend/app/controllers/events/index.js
+++ frontend/app/controllers/events/index.js
@@ -3,5 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   session: Ember.inject.service(),
   totalTicketsArray: Ember.computed.mapBy('model', 'bookedSeats'),
-  totalTickets: Ember.computed.sum('totalTicketsArray')
+  totalTickets: Ember.computed.sum('totalTicketsArray'),
+  totalSeatsArray: Ember.computed.mapBy('model', 'seats'),
+  totalSeats: Ember.computed.sum('totalSeatsArray')
 });
