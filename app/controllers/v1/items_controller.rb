97134b0cef04c7ff0ddccac9b7c87cce3569285a
jkcarlsson85 <jkcarlsson85@gmail.com>
2019-09-24 23:30:10 +0200

    Ensure bookings cannot be accessed without admin access rights

--- app/controllers/v1/items_controller.rb
+++ app/controllers/v1/items_controller.rb
@@ -1,3 +1,4 @@
 class V1::ItemsController < V1::BaseController
   before_action :authenticate_admin_from_token! 
+  before_action :authenticate_admin! , except: [:index, :show, :get_related_resource]
 end
